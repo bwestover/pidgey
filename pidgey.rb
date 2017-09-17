c613c32ebf027bca9239b4764c4f258c1e7943ee
bwestover <bwestover@github.com>
2017-09-17 08:56:15 -0700

    Add a rake file and have failing tests

--- pidgey.rb
+++ pidgey.rb
@@ -3,7 +3,10 @@ class Pidgey
 
   def self.calc(pidgeys, candy, aggressive_trade=false)
     total_evolutions = 0
-    while (candy / COST) > 0
+    loop_cnt = 0
+    while (candy / COST) >= 1
+      loop_cnt +=1
+      puts "Begin loop #{loop_cnt}, #{pidgeys} pidgeys, #{candy} candy."
       # Evolutions are candy divided by the cost
       evolutions = candy / COST
       # Calculate the remaining candy.
@@ -19,14 +22,18 @@ class Pidgey
         candy += evolutions
         # If we have don't already have enough candy for another evolution
         # but we do have enough extra pidgeys to make another evolution, trade them in.
-        if ((candy / COST < 1) && ((pidgeys - 1) + candy / COST > 1))
+        puts "Should trade?: pidgeys:#{pidgeys} candy:#{candy} Calculated: #{((pidgeys - 1) + candy) / COST}"
+        if ((candy / COST < 1) && (((pidgeys - 1) + candy) / COST > 1))
+          puts "Not enough candy, but I can trade some in with #{candy} candy and #{pidgeys} pidgeys."
           until (candy == COST)
             candy += 1
             pidgeys -= 1
           end
         end
       end
+      puts "End loop #{loop_cnt}, #{pidgeys} pidgeys, #{candy} candy and #{total_evolutions} evolutions."
     end
     {"evolutions" => total_evolutions, "pidgeys" => pidgeys, "candy" => candy}
   end #Calc method
 end #Pidgey class
+#puts Pidgey.calc(27, 137, true)
